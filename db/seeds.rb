885a7e5c0d39a5d012bdd5a833d43dce2a780351
Unknown <nicolas.bertin0@gmail.com>
2018-07-26 19:10:58 +0200

    functionnal app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,24 @@
 require 'faker'
 
+5.times do
+  city = City.create(city_name: Faker::LeagueOfLegends.location)
+end
+
 10.times do
-  doc = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: Faker::Military.marines_rank, postal_code: Faker::Address.postcode)
+  spec = Specialty.create(name: Faker::Military.marines_rank)
+end
+
+10.times do
+  doc = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, postal_code: Faker::Address.postcode, city_id: City.all.sample.id)
 end
 
 20.times do
-  pat = Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  pat = Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city_id: City.all.sample.id)
 end
 
 50.times do
   a = Appointment.new
-  a.assign_attributes({date: Faker::Date.between(2.days.ago, Date.today)})
+  a.assign_attributes({date: Faker::Date.between(2.days.ago, Date.today), city_id: City.all.sample.id})
   a.patient = Patient.all.sample
   a.doctor = Doctor.all.sample
   a.save
